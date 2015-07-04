de5df6ba19a12d8519ad972187f219f30373c0d1
masaki_nukaga <masaki.nukaga1209@gmail.com>
2015-07-04 12:50:29 +0900

    spec5: IDでレシピを指定できるように変更

--- print_recipes.rb
+++ print_recipes.rb
@@ -1,7 +1,21 @@
+
+def display_all_recipes(recipes)
+  recipes.each_with_index do |recipe, recipe_id|
+    puts "#{recipe_id}: #{recipe}"
+  end
+end
+
+def display_target_recipe(recipes, target_id)
+  puts "#{target_id}: #{recipes[target_id]}"
+end
+
+target_recipe_id = ARGV[0]
 File.open('recipes.txt') do |file|
   recipes = file.read.split("\n")
 
-  recipes.each_with_index do |recipe, recipe_id|
-    puts "#{recipe_id}: #{recipe}"
+  if target_recipe_id
+    display_target_recipe(recipes,target_recipe_id.to_i)
+  else
+    display_all_recipes(recipes)
   end
 end
