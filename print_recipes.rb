1c3c98702815d8dc9948b0cca87d17a38f045199
masaki_nukaga <masaki.nukaga1209@gmail.com>
2015-07-04 11:50:27 +0900

    spec:5 レシピごとにIDを割り振る

--- print_recipes.rb
+++ print_recipes.rb
@@ -1,5 +1,7 @@
 File.open('recipes.txt') do |file|
-  file.read.each_line do |recipe|
-    puts recipe
+  recipes = file.read.split("\n")
+
+  recipes.each_with_index do |recipe, recipe_id|
+    puts "#{recipe_id}: #{recipe}"
   end
 end
