68c0e234be23c0ab211fcefd8dd03a4cf7e71366
yorisilo <yorisilo@gmail.com>
2015-07-04 14:51:58 +0900

    spec8: ユーザー名に紐付いたrecipeファイルを表示する

--- print_recipes.rb
+++ print_recipes.rb
@@ -1,3 +1,5 @@
+# coding: utf-8
+# id は 0から
 
 def display_all_recipes(recipes)
   recipes.each_with_index do |recipe, recipe_id|
@@ -7,14 +9,21 @@ end
 
 def display_target_recipe(recipes, target_id)
   puts "#{target_id}: #{recipes[target_id]}"
+  # p recipes[target_id]
 end
 
-target_recipe_id = ARGV[0]
-File.open('recipes.txt') do |file|
-  recipes = file.read.split("\n")
+def usr_link_recipes(usr)
+  puts "ユーザー名: " + usr
+end
 
+usr_name = ARGV[0]
+target_recipe_id = ARGV[1]
+
+File.open('recipes.txt') do |file|
+  recipes = file.readlines
+  usr_link_recipes(usr_name)
   if target_recipe_id
-    display_target_recipe(recipes,target_recipe_id.to_i)
+  display_target_recipe(recipes, target_recipe_id.to_i)
   else
     display_all_recipes(recipes)
   end
